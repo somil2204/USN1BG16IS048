804f9964dc3f0cef9a447168a3c9a9d6559785d8
Somil Agrawal <2204.somil@gmail.com>
2019-02-28 14:53:39 +0530

    somil

---
+++ script.js
@@ -0,0 +1,26 @@
+//decision making
+/*var age=18;
+if(age<18){
+    console.log("minor");
+}else if(age>=18){
+    console.log("major");
+
+}
+//con?true:false
+age>18?console.log("major"):console.log("minor");
+
+function sachin(firstname,lastnmae){
+    console.log(firstname+''+lastname);
+
+}
+sachin('somil','agrawal');
+
+//array
+var names=['sachin','ankita','avyaan'];
+console.log(names[2]);
+console.log(names.length);
+names.push("Shrihaan");
+names.unshift("rajesh");
+console.log(names);*/
+//challenge 2
+
